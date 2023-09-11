25a24293183673837f244e9f067a540724446800
Baher Tamer <baher.appledev@gmail.com>
2023-09-11 18:41:50 +0300

    Refactoring and Fix name with space issues

--- Skycast/Skycast/Services/CityAPI.swift
+++ Skycast/Skycast/Services/CityAPI.swift
@@ -10,14 +10,19 @@ import Foundation
 
 struct CityAPI {
     static func fetchCity(for name: String) -> AnyPublisher<[City], Error> {
-            let url = URL(string: "http://api.openweathermap.org/geo/1.0/direct?q=\(name.capitalized)&limit=1&appid=\(API.key)")!
-
-            return URLSession.shared
-                .dataTaskPublisher(for: url)
-                .tryMap(getData)
-                .decode(type: [City].self, decoder: JSONDecoder())
-                .eraseToAnyPublisher()
+        let cityName = name.capitalized.replacingOccurrences(of: " ", with: "-")
+        
+        guard let url = URL(string: "http://api.openweathermap.org/geo/1.0/direct?q=\(cityName)&limit=1&appid=\(API.key)") else {
+            print("DEBUG: Invalid weather URL,")
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
         }
+        
+        return URLSession.shared
+            .dataTaskPublisher(for: url)
+            .tryMap(getData)
+            .decode(type: [City].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
     
     private static func getData(data: Data, response: URLResponse) throws -> Data {
         guard
