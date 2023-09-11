53edac6fbfe8e47d6a2bf36000e8ca9048be8fbd
Baher Tamer <baher.appledev@gmail.com>
2023-09-11 15:52:32 +0300

    Create City Model

---
+++ Skycast/Skycast/Models/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  Skycast
+//
+//  Created by Baher Tamer on 11/09/2023.
+//
+
+import Foundation
+
+struct City: Codable {
+    var name: String
+    let lat: Double
+    let lon: Double
+}
