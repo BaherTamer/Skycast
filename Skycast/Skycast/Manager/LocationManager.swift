7c4ae57e8bc52b51725dd8f7db58bc979373982f
Baher Tamer <baher.appledev@gmail.com>
2023-09-11 15:35:02 +0300

    Create LocationManager

---
+++ Skycast/Skycast/Manager/LocationManager.swift
@@ -0,0 +1,51 @@
+//
+//  LocationManager.swift
+//  Skycast
+//
+//  Created by Baher Tamer on 11/09/2023.
+//
+
+import CoreLocation
+import Foundation
+
+class LocationManager: NSObject, ObservableObject {
+    
+    private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocation?
+    
+    static let shared = LocationManager ()
+    override init() {
+        super.init()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+    }
+    
+    func checkLocationAuthorization() {
+        switch manager.authorizationStatus{
+            
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation()
+            self.userLocation = manager.location
+            
+        default:
+            break
+            
+        }
+    }
+    
+    func isLocationServicesDenied() -> Bool {
+        manager.authorizationStatus == .denied
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
