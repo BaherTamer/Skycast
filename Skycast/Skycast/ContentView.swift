492a40632d0ea0b4a5ed5bf797a64ad5a1d7eabb
Baher Tamer <baher.appledev@gmail.com>
2023-09-11 18:04:31 +0300

    Refactoring

--- Skycast/Skycast/ContentView.swift
+++ Skycast/Skycast/ContentView.swift
@@ -5,12 +5,53 @@
 //  Created by Baher Tamer on 11/09/2023.
 //
 
+import CoreLocation
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShowingCitiesView = false
+    
+    @ObservedObject private var locationManager = LocationManager.shared
+    
     var body: some View {
         NavigationStack {
             TodayForecast()
+                .navigationBarTitleDisplayMode(.inline)
+                .sheet(isPresented: $isShowingCitiesView) {
+                    CitiesListView()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        cityNameButton
+                    }
+                }
+                .onChange(of: locationManager.userLocation) { newValue in
+                    controlCitiesSheet(for: newValue)
+                }
+                .onAppear {
+                    controlCitiesSheet(for: locationManager.userLocation)
+                }
+        }
+    }
+    
+    private var cityNameButton: some View {
+        Button {
+            isShowingCitiesView = true
+        } label: {
+            Text("Cairo")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .tint(Color(UIColor.systemBackground))
+        }
+    }
+    
+    // MARK: - Functions
+    private func controlCitiesSheet(for location: CLLocation?) {
+        if location == nil {
+            isShowingCitiesView = true
+        } else {
+            isShowingCitiesView = false
         }
     }
 }
