430372d5420fa38fa1ebd23fff1748d3003d7c82
Baher Tamer <baher.appledev@gmail.com>
2023-09-11 13:34:43 +0300

    Setup Xcode Files

---
+++ Skycast/Skycast/SkycastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkycastApp.swift
+//  Skycast
+//
+//  Created by Baher Tamer on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkycastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
