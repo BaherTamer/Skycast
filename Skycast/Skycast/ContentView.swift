98d375a812e949fc3e72341158420a8484aca33d
Baher Tamer <baher.appledev@gmail.com>
2023-09-12 12:58:31 +0300

    Refactoring

--- Skycast/Skycast/ContentView.swift
+++ Skycast/Skycast/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            TodayForecast(for: city)
+            TodayForecast(city: $city)
                 .navigationBarTitleDisplayMode(.inline)
                 .sheet(isPresented: $isShowingCitiesView) {
                     CitiesListView { city in
