371804db6775193643ee5dfbe86c6a0deccf7f91
Baher Tamer <baher.appledev@gmail.com>
2023-09-13 13:44:36 +0300

    Dark Mode Preferences Configurations

--- Skycast/Skycast/SkycastApp.swift
+++ Skycast/Skycast/SkycastApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct SkycastApp: App {
+    
+    // MARK: Dark Mode Variables
+    @StateObject private var colorSchemeManager = ColorSchemeManager()
+    @AppStorage("SchemeType") private var schemeType: SchemeType = .unspecified
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            
+                // Dark Mode Configurations
+                .environmentObject(colorSchemeManager)
+                .onAppear {
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                    colorSchemeManager.applyColorScheme()
+                }
+                .onChange(of: schemeType) { _ in
+                    colorSchemeManager.applyColorScheme()
+                }
         }
     }
 }
